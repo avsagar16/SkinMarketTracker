c146d40ad341164ed0486efbe17fdf8f2b86bb70
adithya-sagar <107962675+adithya-sagar@users.noreply.github.com>
2025-09-07 23:16:15 -0400

    trial

---
+++ test.py
@@ -0,0 +1,29 @@
+import requests
+#import os
+from dotenv import load_dotenv
+import json
+from pymongo import MongoClient
+
+#url = "https://cat-fact.herokuapp.com"
+#url = "https://csfloat.com/api/v1/listings/<ID>"
+base_url = "https://pokeapi.co/api/v2/"
+cs_base_url = "https://csfloat.com/api/v1/listings"
+
+def get_float_info(name):
+    with open("config.json") as f:
+        config = json.load(f)
+    
+    API_KEY = config["API_KEY"]
+    headers = {"Authorization": API_KEY}
+    response = requests.get(cs_base_url, headers = headers)
+    if response.status_code == 200:
+        data = response.json()
+        print(data)
+        return data
+    else:
+        print(f"Failed to retrieve data {response.status_code}")
+    return cs_base_url
+    pass
+
+get_float_info("check")
+
